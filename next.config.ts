43e09fae3f674d9f20112ae37779afc457d0d577
Peter van Londen <info@worldemp.com>
2025-12-06 15:08:14 +0000

    https://nextjs.org/blog/CVE-2025-66478#required-action
    Check if this is

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverActions: {
+    bodySizeLimit: '1mb', // Or a value appropriate for your app's needs
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
