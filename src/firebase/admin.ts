e6a164a6fd0a7f65c290048294668162e94c578b
Peter van Londen <info@worldemp.com>
2025-12-08 17:24:48 +0000

    Try fixing this error: `Runtime SyntaxError: "undefined" is not valid JS

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -2,8 +2,12 @@
 import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT;
+  if (!serviceAccount) {
+    throw new Error("The FIREBASE_SERVICE_ACCOUNT environment variable is not set.");
+  }
   admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN_KEY!)),
+    credential: admin.credential.cert(JSON.parse(serviceAccount)),
   });
 }
 
